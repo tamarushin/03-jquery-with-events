6794950cb1c6cd8e112dafac3ed0fabaa030eb7a
tamarushin <tamarushin@gmail.com>
2018-03-24 10:50:30 -0700

    called the functions

--- nicholas-tama/scripts/articleView.js
+++ nicholas-tama/scripts/articleView.js
@@ -38,20 +38,22 @@ articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
-      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      // TODONE: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       $('#articles').find('article').hide();
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
       let $show = $(this).val()
       $(`article[data-author = '${$show}']`).fadeIn(750);
 
     } else {
-      // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
+      // TODONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $('#articles').find('article').show();
       $('.template').hide();
     }
     $('#category-filter').val('');
   });
 };
+
+//Do the foloowing much like the articleView.handleAuthorFilter = function()!!!
 
 articleView.handleCategoryFilter = function() {
   // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
